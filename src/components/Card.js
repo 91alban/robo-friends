fe3bb63dd35f441c40e274c15f1e47604497f67f
91alban <aloku93@gmail.com>
2021-05-04 18:12:27 +0200

    finished robofriend app

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ const { name, email, id } = props;
         <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
             <img alt="robot" src={`https://robohash.org/${id}?100x100`} />
             <div>
-                <h1>{name}</h1>
+                <h2>{name}</h2>
                 <p>{email}</p>
             </div>
         </div>
