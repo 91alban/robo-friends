ae4748a071b682aa407fd7b7804a7f20ff8fc80c
91alban <aloku93@gmail.com>
2021-05-04 15:53:02 +0200

    added a searchBox

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import App from './App.js';
 import 'tachyons';
-import {robots, Robots} from './Robots';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+ReactDOM.render(<App />, document.getElementById('root'));
